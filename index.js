69e2765e472f8a7eefff761373520eb3fa4bc27c
Helge Skogly Holm <helge.holm@gmail.com>
2015-06-01 23:08:08 +0200

    Start looking at de booty.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.post('/', function(req, res) {
+  res.send('Hello World! ' + JSON.stringify(req.body));
 });
 
 app.listen(app.get('port'), function() {
