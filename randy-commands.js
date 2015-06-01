667bff7d096c0ec8c85019eadf4036b63784cc38
Helge Skogly Holm <helge.holm@gmail.com>
2015-06-01 23:35:46 +0200

    Command error messages give syntax.

--- randy-commands.js
+++ randy-commands.js
@@ -4,13 +4,13 @@ module.exports = {
   d: function(sidesStr) {
     var sides = parseInt(sidesStr, 10);
     if (!sides)
-      throw new Error('Unrecognized number of sides.');
+      throw new Error('d <sides>');
     return randy.randInt(sides);
   },
 
   choice: function(stuffs) {
     if (!stuffs.length)
-      throw new Error('Must provide list of things to choose from.');
+      throw new Error('choose [item1] [item2]...');
     return randy.choice(stuffs);
   }
 }
