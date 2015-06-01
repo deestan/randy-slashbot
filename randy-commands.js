eaca2c9cf408b9c0b7f3b05e6f14dfea0f1cac48
Helge Skogly Holm <helge.holm@gmail.com>
2015-06-01 23:33:45 +0200

    Added choice command.

--- randy-commands.js
+++ randy-commands.js
@@ -6,5 +6,11 @@ module.exports = {
     if (!sides)
       throw new Error('Unrecognized number of sides.');
     return randy.randInt(sides);
+  },
+
+  choice: function(stuffs) {
+    if (!stuffs.length)
+      throw new Error('Must provide list of things to choose from.');
+    return randy.choice(stuffs);
   }
 }
